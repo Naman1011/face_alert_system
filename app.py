e019f92f7ad9c5e01b3abf076f4a1f2d564697b9
Naman1011 <45920615+Naman1011@users.noreply.github.com>
2020-05-14 14:55:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import pickle
+import numpy as np
+from flask import Flask, jsonify, request 
+
+
+app = Flask(__name__) 
+model = pickle.load(open('try01.pkl', 'rb'))
+
+@app.route('/')
+def home(): 
+	
+		return jsonify({"message" : "Hi! there"}) 
+
+@app.route('/home/predict', methods = ['POST']) 
+def predict(): 
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == '__main__': 
+
+	app.run(debug = True) 
